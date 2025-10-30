1cd5fa2ea46f73dac88a3cc29d5ccb6c05d653c5
rootanvir <tanvir.22464751@gmail.com>
2025-10-30 15:02:03 +0600

    button

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,22 @@
 import React, { FC } from 'react';
-
+import Btn from '@/components/Btn';
 interface Props {
 
 }
 
 const Page: FC<Props> = ({ }) => {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="flex items-center justify-center min-h-screen ">
+      <div className="flex flex-col gap-6">
+        <Btn text="Button" variant="filled" />
+        <Btn text="Button" variant="outline" />
+        <Btn text="Button" variant="outline" image='globe.svg' />
+        <Btn text="Button" />
+        <Btn text="Button" variant="3d" />
+        <Btn text="Button" variant="liquid" />
+      </div>
     </div>
+
   );
 };
 
--- components/Btn.tsx
+++ components/Btn.tsx
@@ -1,12 +1,39 @@
-import React, { FC } from 'react';
+import React, { ReactNode } from 'react';
+import '../app/globals.css';
 
-interface Props {
-  
+interface BtnProps {
+  text: string;
+  variant?: 'filled' | 'outline' | '3d' | 'liquid' | 'default';
+  className?: string;
+  image?: string;
 }
 
-const Btn: FC<Props> = ({  }) => {
+const Btn: React.FC<BtnProps> = ({ text, variant, className = '', image }) => {
+  let base =
+    'px-3 py-2 rounded-md cursor-pointer flex items-center justify-center gap-2 transition-all duration-150 font-medium';
+
+  let btnStyle = '';
+
+  if (variant === 'filled') {
+    btnStyle =
+      'bg-blue-500 text-white hover:bg-blue-600 shadow-md active:scale-95';
+  } else if (variant === 'outline') {
+    btnStyle =
+      'border border-gray-500 text-gray-800 hover:bg-gray-100 active:scale-95';
+  } else if (variant === '3d') {
+    btnStyle =
+      'text-center bg-blue-600 text-white font-semibold px-6 py-3 rounded-lg shadow-[0_6px_0_#1e40af] hover:bg-blue-700 hover:-translate-y-0.5 active:translate-y-1 active:shadow-[0_2px_0_#1e40af]';
+  } else if (variant == 'liquid') {
+    btnStyle = ''  }
+  else {
+    btnStyle = 'text-gray-700 hover:text-gray-500';
+  }
+
   return (
-    <div>Btn</div>
+    <button className={`${base} ${btnStyle} ${className}`}>
+      {image && <img src={image} alt="icon" className="w-5 h-5" />}
+      <span className="text-center">{text}</span>
+    </button>
   );
 };
 
